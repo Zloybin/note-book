4ceec135737b045cb42a395c32b66886a146883e
Zlobin Yevhen <zlobinyevhen0707@gmail.com>
2024-02-02 13:55:56 +0100

    Init commit

---
+++ src/main/java/dev/ezlobin/notebook/NoteBookApplication.java
@@ -0,0 +1,13 @@
+package dev.ezlobin.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ezlobin/notebook/NoteBookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ezlobin.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
